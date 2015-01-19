664c3163b81979938eff6fc869177b598f853b55
Joel Pickup <joelpickup@hotmail.co.uk>
2015-01-19 12:12:04 +0000

    added associations between lesson and user, and migrated the lesson generation migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150119115904) do
+ActiveRecord::Schema.define(version: 20150119120910) do
+
+  create_table "lessons", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "duration"
+    t.integer  "price"
+    t.integer  "instructor_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
