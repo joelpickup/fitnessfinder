5fc7052fc95052f4e65ffaf431b7ec67e09d9a7a
Joel Pickup <joelpickup@hotmail.co.uk>
2015-01-20 15:01:40 +0000

    fixed register and edit form to allow necessary fields to be put in, then updated the devise strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,16 +9,13 @@ class UsersController < ApplicationController
   def show
     @user = set_user
 
-
-
   end
 
-
   def update
+    raise
     @user.update(user_params)
     respond_with(@user)
   end
-
 
   private
     def set_user
@@ -26,7 +23,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :avatar, :avatar_cache, :remove_avatar, :email, :description, :duration, :price, :instructor_id)
+      params.require(:user).permit(:first_name, :last_name, :role, :avatar, :avatar_cache, :remove_avatar, :email, :description, :duration, :price, :instructor_id)
     end
-
 end
