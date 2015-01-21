77ad5f8a2440ca181e0fb6e9c73a0ba712a43c0d
Antonio <chefeusebio@gmail.com>
2015-01-21 23:22:04 +0000

    removing unnecessary likes comented out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,21 @@
 class UsersController < ApplicationController
 
-  # def index
-  #   @users = User.all
-  #   respond_with(@users)
-  # end
 
   def show
       @user = current_user.id
-      # binding.pry
+     
   end
 
   def update
     @user.update(user_params)
     respond_with(@user)
   end
-  # def edit
-
-  # end
-
 
   private
     def set_lesson
       @user = User.find(params[:id])
     end
+    
 
     def user_params
       params.require(:user).permit(:first_name, :last_name, :avatar, :avatar_cache, :remove_avatar, :email, :description, :duration, :price, :instructor_id)
