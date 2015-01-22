10a86ac156b28c02d05b3af2aae9cd94510c9506
Antonio <chefeusebio@gmail.com>
2015-01-22 16:15:42 +0000

    stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    users_show_path(current_user) #your path
+    dashboard_path #your path
   end
   protected
 
